021d218bc70c8237b3d4d3eca98945169e97e889
Mathiyarasip31 <mathiyarasip31@gmail.com>
2023-07-14 16:04:22 +0530

    Initial commit done

---
+++ src/SubSequence.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SubSequence {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String s= scanner.nextLine();
+        System.out.println(sum(s));
+    }
+    public static int sum(String s)
+    {
+        char[] ch=s.toCharArray();
+        int count=0,count1=0;
+        for(int i=0;i<ch.length;i++)
+        {
+            if(ch[i]=='A')
+            {
+                count++;
+            }
+            else if(ch[i]=='G')
+            {
+                count1+=count;
+            }
+        }return count1;
+    }
+}
